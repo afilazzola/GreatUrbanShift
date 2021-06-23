cba0059ac19e2adf8e7252ae85af776762c6ddb9
afilazzola <alex.filazzola@outlook.com>
2021-06-23 09:52:13 -0400

    organized polygons. Got the IUCN status of the target species in study

--- scripts/speciesSelection/getIUCN.r
+++ scripts/speciesSelection/getIUCN.r
@@ -7,3 +7,12 @@ library(tidyverse)
 ## load species data
 sppList <- read.csv("data//cityData//UpdatedSpeciesList.csv")
 
+## load IUCN status
+IUCN <- read.csv("data//IUCN//assessments.csv", stringsAsFactors = F)
+IUCNreduced <- IUCN %>% select(assessmentId, internalTaxonId, species = scientificName, redlistCategory, 
+                               populationTrend,yearPublished )
+
+## Join status to full species list
+sppIUCN <- merge(sppList, IUCNreduced, all.x=T)
+
+write.csv(sppIUCN, "data//IUCNspeciesList.csv", row.names=FALSE)
